6348382d7b69855fbc7467923fa3dd7e7e8a6f8f
bryan.bar <bryan.bar92@gmail.com>
2022-05-14 22:45:36 -0500

    funcionando despliegue de imagen

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 const images = document.querySelectorAll('.img');
 const containerImage = document.querySelector('.container-img');
+const imageContainer = document.querySelector('.img-show');
 
 images.forEach(image =>{
-    image.addEventListener('clic', ()=>{
-        containerImage.classList.toggle('move');
-        console.log(image.getAttribute('src'))
+    image.addEventListener('click', ()=>{
+        addImage(image.getAttribute('src'),image.getAttribute('alt'));
     })
+})
+
+const addImage = (srcImage, altImage)=>{
+    containerImage.classList.toggle('move');
+    imageContainer.src = srcImage;
+}
+
+containerImage.addEventListener('click', ()=>{
+    containerImage.classList.toggle('move');
+
 })
